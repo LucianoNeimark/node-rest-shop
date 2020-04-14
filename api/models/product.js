5a9cb19cd40659bce50f1057f166c2b268a25dd2
LucianoNeimark <38950566+LucianoNeimark@users.noreply.github.com>
2020-04-13 22:45:07 -0300

    Required data and proper response message

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({ //Un Schema es el diseño del objeto
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number
+    name: {type: String, required: true},
+    price: {type: Number, required: true}
 })
 
 module.exports = mongoose.model('Product', productSchema); //El model es el constructor del objeto
--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,22 @@ router.get('/', (req, res, next) =>{
     Product.find()
     .exec()
     .then(docs =>{
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return{
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: "GET",
+                        url: 'http://localhost:3000/products/' + doc._id
+                    }
+                }
+            })
+        }
         console.log(docs);
-        res.status(200).json(docs); // Mostrar en la respuesta el codigo 200 (todo ok) y docs que es la respuesta al GET
+        res.status(200).json(response); // Mostrar en la respuesta el codigo 200 (todo ok) y docs que es la respuesta al GET
     })
     .catch(err => {
         console.log(err);
@@ -17,19 +31,26 @@ router.get('/', (req, res, next) =>{
             error: err
         })
     })
-}); 
+});    
 router.post('/', (req, res, next) =>{
     const product = new Product({  //POST de un nuevo modelo a la database cumpliendo con el schema
         _id: new mongoose.Types.ObjectId,
         name: req.body.name,
         price: req.body.price
     });
-
     product.save().then(result => {  //Guardar en la db el producto creado por el POST
         console.log(result);
         res.status(201).json({
-            message: 'Handling POST requests to /products',
-            createdProduct: result
+            message: 'Created Product succesfully',
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request:{
+                    type: "GET",
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
         });
     }).catch(err => {
         console.log(err);
@@ -46,7 +67,17 @@ router.get('/:productId', (req, res, next) => {
         console.log(doc);
         if (doc)
         {
-            res.status(200).json(doc);
+            res.status(200).json({
+                product: {
+                    name: doc.name,
+                    price: doc.price,
+                    id: doc._id,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:3000/products/" + doc._id
+                    }
+                }
+            });
         } else{
             res.status(404).json({message: 'No valid entry found for provided ID '})
         }
@@ -61,7 +92,6 @@ router.get('/:productId', (req, res, next) => {
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
-
     for(const ops of req.body)
     {
         updateOps[ops.propName] = ops.value; 
@@ -70,7 +100,13 @@ router.patch('/:productId', (req, res, next) => {
     .exec()
     .then(result => {
         console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product Updated',
+            request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + id
+            }
+        });
     })
     .catch(err => {
         console.log(err);
@@ -85,7 +121,10 @@ router.delete('/:productId', (req, res, next) => {
     .exec()
     .then(result => 
         {
-            res.status(200).json(result);
+            res.status(200).json({
+                "message": "Product deleted",
+                "Result": result
+            });
         })
     .catch(err => {
         console.log(err);
